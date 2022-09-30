6c90fa248dfe1e4f7924cc724c492091fbb19cdd
gummabhargavi <108506501+gummabhargavi@users.noreply.github.com>
2022-09-30 15:30:57 +0530

    Add files via upload

---
+++ HtmlDemo/src/main/java/com/infinitehtml/App.java
@@ -0,0 +1,13 @@
+package com.infinitehtml;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
